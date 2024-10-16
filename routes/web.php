47ad13794ac291ef1c71786f37c999203f6923fe
orlianheru <541231097@student.smktelkom-pwt.sch.id>
2024-10-16 14:15:48 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index'); //merujuk ke file welcome.blade.php di folder resource view
+});
+
+Route::get('/welcome', function () {
+    return view('welcome'); //merujuk ke file welcome.blade.php di folder resource view
 });
